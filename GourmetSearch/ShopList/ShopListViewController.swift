05513809248e8c19684ff24eae9307f9f698682b
佐藤恒 <koh.satoh.kyotouniv@gmail.com>
2017-10-23 19:20:51 +0900

    table view, table view cellの作成

---
+++ GourmetSearch/ShopList/ShopListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ShopListViewController.swift
+//  GourmetSearch
+//
+//  Created by 佐藤恒 on 2017/10/23.
+//  Copyright © 2017年 佐藤恒. All rights reserved.
+//
+
+import UIKit
+
+class ShopListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+// MARK: - UITableViewDelegate,UITableViewDataSource
+extension ShopListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ShopListItem") as! ShopListItemTableViewCell
+            cell.name.text = "\(indexPath.row)"
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
